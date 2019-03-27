2196f4ec4dc57c0d9248f9d0ea3175c686b5e43f
S. Michael Hussain <smhussain_1@yahoo.com>
2019-03-26 17:25:19 -0700

    datafile updated and complete w 7 objects

---
+++ storeData.js
@@ -0,0 +1,36 @@
+let data = [
+    {number: 1,
+    question: "Who is the highest scoring Chess Champion in History?",
+    answers: ['Bobby Fischer', 'Anatoly Karpov', 'Magnus Carlsen', 'Garry Kasparov'],
+    correctAns: 'Magnus Carlsen'},
+
+    {number: 2,
+    question: 'What is the highest title a competitive Chess Player can attain?',
+    answers: ['International Grandmaster', 'World Master', 'World Champion', 'Grandmaster'],
+    correctAns: 'World Champion'},
+
+    {number: 3,
+    question: 'Who did Bobby Fischer play against in Reykevik in one of the most watched chess battles in the history of the game?',
+    answers: ['Anatoly Karpov', 'Vladimir Kramnik', 'Boris Spassky', 'Emanuel Lasker'],
+    correctAns: 'Boris Spassky'},
+
+    {number: 4,
+    question: 'José Raúl Capablanca is best known for what style of chess, in which he has been described as the ‘absolute master’ of?',
+    answers: ['The Orangutang Opening', 'Blanche (always winning when playing white, which enjoys the first move advantage)', 'Blitz chess (games of 5 minutes or less)', 'Blind chess (where players are blindfolded, keeping the game in their minds and calling out moves.'],
+    correctAns: 'Blitz chess (games of 5 minutes or less)'},
+
+    {number: 5,
+    question: 'Magnus Carlsen was how old when he won the title of Grandmaster?',
+    answers: [12, 13, 15, 19],
+    correctAns: 13},
+	
+    {number: 6,
+    question: 'The sequence of moves which plays out as follows:  1.d4 e6 2.e4 d5, is called…..:',
+    answers: ['The French Defense', 'The Sicilian', 'The Slav Opening', 'The Black Queen’s Gambit'],
+    correctAns: 'The French Defense'},
+
+    {number: 7,
+    question: 'Who is considered to be “the Father of Chess”?',
+    answers: ['Mikhail Tal', 'Bobby Fischer', 'Wilhelm Steinitz', 'Mikhail Botvinnik'],
+    correctAns: 'Wilhelm Steinitz'}
+    ]
