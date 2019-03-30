83c6d94e83a8a1db9f4961ab1f6b8bb0facc92c4
S. Michael Hussain <smhussain_1@yahoo.com>
2019-03-30 09:30:41 -0700

    pre-finalization before respons design commit

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-// count and tracking functions:
-// should these be global variables?
+
+
+// initializing count and tracking functions......:
 
 let score = 0;
 let questionNum = 1;
 let questionsAnswered = 0;
 
-// 
+// question and score iterators......:
 
+function iterateQuestion() { //do we need to pass in the variable as an argument?
+    questionNum++;
+  }
+  
+function iterateScore() {
+    score++;
+  }
+
+function iterateQuestionsAnswered() {
+    questionsAnswered++;
+}
+
+
+  //----- user interation function below.....:
 
 function answerCheck(userAnswer, data) {
     let x = (questionNum -1);
-    if (userAnswer === data[x].correctAns) {
+    iterateQuestion();
+    iterateQuestionsAnswered(); 
+     if (userAnswer === data[x].correctAns) {
+        iterateScore();    
         return true;
-    }
-    else {return false;
+    }else{
+        return false;
     };
-    
-    
+}
 
-function response() {
-    // will issue a response to the user based on whether he/she gets the answer right or wrong.
-    // will display score from function score
-    // will promt the user to go to the next question, or exit application
-
-    // psuedocode: If function correct boolean value === True ${header}.html("Correct etc"), else 
-    // ${header}.html{"Sorry - Incorrect etc"}.  Display Question Number and Score function.
-    // Will display Next button.  Display Correct Answer: If answers[answerx].array.object[key] === true, 
-    // then highlight this label and dim the others.
-};
+// alternate code...:
+// return (userAnswer === data[x].correctAns)
+//if(){
+//}else{
+//}
 
 
+   
+//----------------------- form functions below.......:
 
-    function questionForm(questionNum, data, score) {
+function questionForm(questionNum, data, score, questionsAnswered) {
         let z=(questionNum-1);
     return `  
     <main> <!-- semantics role to be added -->
@@ -72,30 +86,81 @@ function response() {
                 <span id="question-count">Question: ${questionNum}/10</span>
                 <span id="score-count">Score: ${score}/${questionsAnswered}</span>
             </section>
-   `
-};
+   `;
+}
 
+
+  function correctForm(questionNum, data) { // needs to write new HTML and update score and questionNum
+    let z=(questionNum-1) // need to make sure this figure is current when function runs
+   return `
+   <main> 
+   <div class="mainmessage">
+   <header>Great Job - You got it Right!</header>
+   </div>
+          <!-- semantics role to be added -->
+       <form>
+                <fieldset>
+                       <legend>Correct Answer</legend>
+                           <label>
+                           <input type="radio" class="answer1" name="option"></input>
+                           <span class="answers">${data[z].correctAns}</span>
+                           </label> 
+                       
+                </fieldset>
+               </form>
+   
+               <button id="next">Next</button>
+   </main>            
+   `;
+  }
+
+  function incorrectForm(questionNum, data) { // needs to write new HTML and update score and questionNum
+    let z=(questionNum-1) // need to make sure this figure is current when function runs
+   return `
+   <main> 
+   <div class="mainmessage">
+   <header>Sorry....</header>
+   </div>
+          <!-- semantics role to be added -->
+       <form>
+                <fieldset>
+                       <legend>Correct Answer</legend>
+                           <label>
+                           <input type="radio" class="answer1" name="option"></input>
+                           <span class="answers">${data[z].correctAns}</span>
+                           </label> 
+                       
+                </fieldset>
+               </form>
+   
+               <button id="js-submit-button">Submit</button>
+   </main>            
+   `;
+  }
+  
 function finalScore() {
     // will display the final score of the user and thank him/her for playing.  
-    // Offer to hit buttons to re-play or close application.
+    // Offer to hit buttons to re-play
     // psuedocode:  ${header}.html("Congratulations your Final Score is ${functionScore})
     // "Please play again or Exit!"
     // Display Play Again button and Exit button
 
 
-};
+}
 
+
+  //---------------------------------------- buttons below--------------
 function replayButton() {
     // will register the replay button hit by user and re-start quiz from beginning.
-};
+}
 
 function startButton() {
     // will register button hit by user and start the application with Question 1.
-    // psuedocode:  When user clicks on start, the function nextQuestion will be called, taking the User to its screen.
-    $('#start').on('click', function () {
-        return nextQuestion;
+    // psuedocode:  When user clicks on start, the function questionForm() will be called, taking the User to its screen.
+    $('#start').on('click', function() {
+        questionForm(); 
     });
-};
+}
 
 function submitButton() {
     // will compare the users answer to the correct answer and determine if right or wrong.
@@ -107,15 +172,30 @@ function submitButton() {
     $('main').on('click', '#js-submit-button', function (event) {
         event.preventDefault()
         const userAnswer = $('input:checked').siblings('span');
-        const userCorrect = answerCheck(userAnswer, data);
+        const userCorrect = answerCheck(userAnswer, data); 
         if (userCorrect) {
-            correctFrom();
-        } else {incorrectForm();
+            correctForm();
+        }else{
+            incorrectForm();
         }
     });
-};
+} 
+
+function handleButtons() {
+    StartButton();
+    SubmitButton();
+    //handleNextButton();
+    //handleRestartButton();
+  }
+  
+handleButtons();
+
+
+//--------------------------------------------buttons above-------------------
+
 
 /* from other project----->
+
 function handleSubmitButton() {
     $('#container').on('click', '#js-submit-button', function(event) {
       event.preventDefault()
@@ -130,11 +210,3 @@ function handleSubmitButton() {
       }
     });
   }  */
-
-  function correctForm() { // needs to write new HTML and update score and questionNum
-
-  };
-
-  function incorrectForm() {
-
-  };
