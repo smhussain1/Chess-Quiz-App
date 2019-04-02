5b18d2fbb6ab15e1957a8c0c2f8c95968880d7d8
S. Michael Hussain <smhussain_1@yahoo.com>
2019-04-02 14:28:23 -0700

    final with accessavility and responsive checked

--- index.js
+++ index.js
@@ -45,31 +45,34 @@ function questionForm(questionNum, data, score) {
     return `  
     <main role='main'> 
         <div class="mainmessage">
-            <header>${data[z].question}</header>
+            <header>
+                <h1>${data[z].question}</h1>
+            </header>
         </div>
        
             <form>
             <fieldset>
                     <legend>Select the Right Answer</legend>
-                        <label> 
-                        <input type="radio" class="answer1" name="option" checked></input>
+                        <label for="answer1" aria-label="answer1">
+                        <input type="radio" id="answer1" class="answer1" name="option" checked></input>
                         <span class="answers">${data[z].answers[0]}</span>
                         </label> 
                     
-                        <label>
-                        <input type="radio" class="answer1" name="option"></input>
+                        <label for="answer2" aria-label="answer2">
+                        <input type="radio" id="answer2" class="answer1" name="option"></input>
                         <span class="answers">${data[z].answers[1]}</span>
                         </label> 
                     
-                        <label>
-                        <input type="radio" class="answer1" name="option"></input>
+                        <label for="answer3" aria-label="answer3">
+                        <input type="radio" id="answer3" class="answer1" name="option"></input>
                         <span class="answers">${data[z].answers[2]}</span>
                         </label> 
                         
-                        <label>
-                        <input type="radio" class="answer1" name="option"></input>
+                        <label for="answer4" aria-label="answer4">
+                        <input type="radio" id="answer4" class="answer1" name="option"></input>
                         <span class="answers">${data[z].answers[3]}</span>
                         </label> 
+                        
                 </fieldset>
                 </form>
 
@@ -90,20 +93,20 @@ function correctForm(questionNum, data) { // needs to write new HTML and update 
     iterateQuestion();
     return `
    <main role='main'> 
-   <div class="mainmessage">
-   <header>Great Job - You got it Right!</header>
-   </div>
-          <!-- semantics role to be added -->
-        <form>
+        <div class="mainmessage">
+            <header>
+                <h1>Great Job - You got it Right!</h1>
+            </header>
+        </div>
+            <form>
                 <fieldset>
                        <legend>Correct Answer</legend>
-                           <label>
-                           <input type="radio" class="answer1" name="option" checked></input>
+                           <label for="correctAnswer" aria-label="correctAnswer">
+                           <input type="radio" id="correctAnswer" class="answer1" name="option" checked></input>
                            <span class="answers">${data[z].correctAns}</span>
                            </label> 
-                       
                 </fieldset>
-        </form>
+            </form>
    
                <button id="next">Next</button>
 
@@ -120,22 +123,22 @@ function lastCorrectForm(questionNum, data) { // needs to write new HTML and upd
     iterateQuestion();
     return `
    <main role='main'> 
-   <div class="mainmessage">
-   <header>Great Job - You got it Right!</header>
-   </div>
-          <!-- semantics role to be added -->
+        <div class="mainmessage">
+            <header>
+                <h1>Great Job - You got it Right!</h1>
+            </header>
+        </div>
         <form>
-                <fieldset>
-                       <legend>Correct Answer</legend>
-                           <label>
-                           <input type="radio" class="answer1" name="option" checked></input>
-                           <span class="answers">${data[z].correctAns}</span>
-                           </label> 
-                       
-                </fieldset>
+            <fieldset>
+                <legend>Correct Answer</legend>
+                    <label for="correctAnswer" aria-label="correctAnswer">
+                        <input type="radio" id="correctAnswer" class="answer1" name="option" checked></input>
+                        <span class="answers">${data[z].correctAns}</span>
+                    </label> 
+            </fieldset>
         </form>
    
-               <button id="finish">Finish</button>
+            <button id="finish">Finish</button>
 
             <section id="status-bar">
                <span id="question-count">Question: ${questionNum}/7</span>
@@ -149,21 +152,22 @@ function lastCorrectForm(questionNum, data) { // needs to write new HTML and upd
 function incorrectForm(questionNum, data) { // needs to write new HTML and update score and questionNum
     let z=(questionNum-1); // need to make sure this figure is current when function runs
     iterateQuestion();
+    console.log(data[z].correctAns);
     return `
     <main role='main'> 
-    <div class="mainmessage">
-    <header>Sorry, incorrect....</header>
-    </div>
-          <!-- semantics role to be added -->
+        <div class="mainmessage">
+            <header>
+            <h1>Sorry, incorrect....</h1>
+            </header>
+        </div>
         <form>
-                <fieldset>
-                       <legend>Correct Answer is... </legend>
-                           <label>
-                           <input type="radio" class="answer1" name="option" checked></input>
-                           <span class="answers">${data[z].correctAns}</span>
-                           </label> 
-                       
-                </fieldset>
+            <fieldset>
+                <legend>Correct Answer is... </legend>
+                    <label for="correctAnswer" aria-label="correctAnswer">
+                        <input type="radio" id="correctAnswer" class="answer1" name="option" checked></input>
+                        <span class="answers">${data[z].correctAns}</span>
+                    </label> 
+            </fieldset>
         </form>
    
             <button id="next">Next</button>
@@ -178,22 +182,23 @@ function incorrectForm(questionNum, data) { // needs to write new HTML and updat
   
   function lastIncorrectForm(questionNum, data) { // needs to write new HTML and update score and questionNum
     let z=(questionNum-1); // need to make sure this figure is current when function runs
+    console.log(z);
     iterateQuestion();
     return `
     <main role='main'> 
-    <div class="mainmessage">
-    <header>Sorry, incorrect....</header>
-    </div>
-          <!-- semantics role to be added -->
+        <div class="mainmessage">
+            <header>
+            <h1>Sorry, incorrect....</h1>
+            </header>
+        </div>
         <form>
-                <fieldset>
-                       <legend>Correct Answer is...</legend>
-                           <label>
-                           <input type="radio" class="answer1" name="option" checked></input>
-                           <span class="answers">${data[z].correctAns}</span>
-                           </label> 
-                       
-                </fieldset>
+            <fieldset>
+                <legend>Correct Answer is...</legend>
+                    <label for="correctAnswer" aria-label="correctAnswer">
+                    <input type="radio" id="correctAnswer" class="answer1" name="option" checked></input>
+                    <span class="answers">${data[z].correctAns}</span>
+                    </label> 
+            </fieldset>
         </form>
    
             <button id="finish">Finish</button>
@@ -217,13 +222,14 @@ function finalScoreForm(questionNum) {
     return `
     <main role='main'> 
         <div class="mainmessage">
-        <header>Awesome - Thanks for Playing!</header>
+            <header>
+            <h1>Awesome - Thanks for Playing!</h1>
+            </header>
         </div>
-               <form id="finalForm">
-              
-       </form>    
+        <form id="finalForm">
+        </form>    
                     
-              <button id="replay">Replay!</button>
+            <button id="replay">Replay!</button>
                  
             <section id="status-bar">
                 <span id="question-count">All Done!</span>
@@ -238,17 +244,15 @@ function replayForm() {
     questionNum = 1;
     return `
     <main role='main'>
-    <div class="mainmessage">
-    <header>Ready for the Chess Quiz?!</header>
-    </div>
-        
-    <button id="start">Start!</button>
+        <div class="mainmessage">
+            <header>
+            <h1>Ready for the Chess Quiz?!</h1>
+            </header>
+        </div>
+      
+        <button id="start">Start!</button>
 
     </main>
-    
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="index.js"></script>
-    <script type="text/javascript" src="storeData.js"></script>
     `;
     }
 
@@ -257,7 +261,7 @@ function replayForm() {
 function startButton() {
     // will register button hit by user and start the application with Question 1.
     // psuedocode:  When user clicks on start, the function questionForm() will be called, taking the User to its screen.
-    $('#start').on('click', function() {
+    $('main').on('click','#start', function() {
         $('main').html(questionForm(questionNum, data, score)); 
     });
 }
@@ -279,18 +283,22 @@ function submitButton() {
         event.preventDefault()
         const userAnswer = $('input:checked').siblings('.answers');
         const userCorrect = answerCheck(userAnswer, data); 
-        if (userCorrect==true && questionNum===7) {
-            $('main').html(lastCorrectForm(questionNum, data));
-        }if(userCorrect==false && questionNum===7) {
-            $('main').html(lastIncorrectForm(questionNum, data));
-        }if(userCorrect) {
-            $('main').html(correctForm(questionNum, data));
-        }else{
-            $('main').html(incorrectForm(questionNum, data));
-            
-        }
-    });
-} 
+    console.log(userCorrect);
+    console.log(questionNum);
+
+        if(questionNum===7){
+            if(userCorrect){
+                $('main').html(lastCorrectForm(questionNum, data));
+            }else{
+                $('main').html(lastIncorrectForm(questionNum, data));
+            }}
+        else if(userCorrect) {
+                $('main').html(correctForm(questionNum, data));
+            }else{ 
+                $('main').html(incorrectForm(questionNum, data));
+            }
+        });
+    }
 
 function finishButton() {
     $('main').on('click', '#finish', function() {
@@ -313,4 +321,3 @@ function handleButtons() {
   }
   
 handleButtons();
-
